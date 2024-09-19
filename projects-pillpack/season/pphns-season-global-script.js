56ddb44032571251196ae0531021e09b9ec789aa
Setnour6 <set.nour6@gmail.com>
2024-09-18 20:38:40 -0400

    Improve Seasons and Menu Pages
    - All season pages will have a grey background with bubbles flying slowly upwards.
    - Added a Season 4 button
    - Made sure some things are not accessible to prevent page missing errors.

---
+++ projects-pillpack/season/pphns-season-global-script.js
@@ -0,0 +1,39 @@
+const bubbleCount = 20;
+const smallBubbleCount = 60;
+const body = document.body;
+
+for (let i = 0; i < bubbleCount; i++) {
+    const bubble = document.createElement('div');
+    const size = Math.random() * 50 + 10;
+    const speed = Math.random() * 30 + 20;
+    const startX = Math.random() * 100;
+    const startY = Math.random() * 100;
+
+    bubble.classList.add('bubble');
+    bubble.style.width = `${size}px`;
+    bubble.style.height = `${size}px`;
+    bubble.style.left = `${startX}vw`;
+    bubble.style.top = `${startY}vh`;
+    bubble.style.animationDuration = `${speed}s, ${speed}s`;
+    bubble.style.animationDelay = `-1s, -1s`;
+
+    body.appendChild(bubble);
+}
+
+for (let i = 0; i < smallBubbleCount; i++) {
+    const bubble = document.createElement('div');
+    const size = Math.random() * 20 + 5;
+    const speed = Math.random() * 260 + 140;
+    const startX = Math.random() * 100;
+    const startY = Math.random() * 100;
+
+    bubble.classList.add('bubble');
+    bubble.style.width = `${size}px`;
+    bubble.style.height = `${size}px`;
+    bubble.style.left = `${startX}vw`;
+    bubble.style.top = `${startY}vh`;
+    bubble.style.animationDuration = `${speed}s, ${speed}s`;
+    bubble.style.animationDelay = `-1s, -1s`;
+
+    body.appendChild(bubble);
+}
